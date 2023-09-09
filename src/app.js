20f3ac5dda4dc619445a54181798e3c28ce5d03f
erconi <nicoariguti9@gmail.com>
2023-09-09 16:34:18 -0500

    chakra

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import Home from './components/Home';
 import Tareas from './components/Tareas';
 import SobreNosotros from './components/SobreNosotros';
 import Menu from './components/Menu';
-import * as React from "react"
 import { ChakraProvider } from "@chakra-ui/react"
 
 const listViewRouter = require('./list-view-router');
@@ -12,28 +11,23 @@ const listEditRouter = require('./list-edit-router');
 
 const App = () => {
   return (
-    <Router>
-      <Menu />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/tareas">
-          <Tareas />
-        </Route>
-        <Route path="/sobrenosotros">
-          <SobreNosotros />
-        </Route>
-      </Switch>
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <Menu />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/tareas">
+            <Tareas />
+          </Route>
+          <Route path="/sobrenosotros">
+            <SobreNosotros />
+          </Route>
+        </Switch>
+      </Router>
+    </ChakraProvider>
   );
 }
-function App({ Component, pageProps }) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
-}
-
 export default App
+
