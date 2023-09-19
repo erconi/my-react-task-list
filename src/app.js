0a95b90e34a507993255ad338f78e3cee78781a3
erconi <nicoariguti9@gmail.com>
2023-09-19 09:08:59 -0500

    componentes de chakra ui

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 import Tareas from './components/Tareas';
 import SobreNosotros from './components/SobreNosotros';
 import Menu from './components/Menu';
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, Box, Button } from "@chakra-ui/react"
 
 const listViewRouter = require('./list-view-router');
 const listEditRouter = require('./list-edit-router');
@@ -14,17 +14,22 @@ const App = () => {
     <ChakraProvider>
       <Router>
         <Menu />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/tareas">
-            <Tareas />
-          </Route>
-          <Route path="/sobrenosotros">
-            <SobreNosotros />
-          </Route>
-        </Switch>
+        <Box borderWidth="1px" borderRadius="lg" overflow="hidden">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/tareas">
+              <Tareas />
+            </Route>
+            <Route path="/sobrenosotros">
+              <SobreNosotros />
+            </Route>
+          </Switch>
+          <Button colorScheme="teal" variant="solid">
+            Click me
+          </Button>
+        </Box>
       </Router>
     </ChakraProvider>
   );
