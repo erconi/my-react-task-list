1cc1d7708cbbc927b669d2140a3ea90ee0c19e64
erconi <nicoariguti9@gmail.com>
2023-09-22 18:14:40 -0500

    servidor.js

--- src/server.js
+++ src/server.js
@@ -11,13 +11,11 @@ let tasks = [
     }
 ];
 
-// Array of predefined users
 const users = [
   { username: 'user1', password: 'password1' },
   { username: 'user2', password: 'password2' }
 ];
 
-// Middleware function to validate JWT token
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
@@ -53,16 +51,15 @@ app.post('/tasks', authenticateToken, (req, res) => {
 app.put('/tasks/:id', authenticateToken, (req, res) => {
   const task = tasks.find(t => t.id === req.params.id);
   if (!task) return res.status(404).send('La tarea con el ID dado no fue encontrada.');
-
   task.title = req.body.title;
   task.completed = req.body.completed;
   res.json(task);
 });
 
+
 app.delete('/tasks/:id', authenticateToken, (req, res) => {
   const task = tasks.find(t => t.id === req.params.id);
   if (!task) return res.status(404).send('La tarea con el ID dado no fue encontrada.');
-
   const index = tasks.indexOf(task);
   tasks.splice(index, 1);
   res.json(task);
@@ -73,16 +70,10 @@ app.get('/tasks/completed/:completed', authenticateToken, (req, res) => {
     res.json(completedTasks);
 });
 
-// Login route
 app.post('/login', (req, res) => {
-  // Authenticate user
   const user = users.find(user => user.username === req.body.username && user.password === req.body.password);
-  
   if (!user) return res.status(401).send('Usuario o contraseña incorrectos.');
-
-  // Create and send JWT token
   const accessToken = jwt.sign({ username: user.username }, process.env.ACCESS_TOKEN_SECRET);
-  
   res.json({ accessToken });
 });
 
